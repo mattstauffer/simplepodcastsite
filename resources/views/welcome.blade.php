8872c526499e679e79e1a29ebae244cd0c2e9175
Matt Stauffer <matt@tighten.co>
2016-02-02 16:06:14 -0500

    add text description too

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -115,6 +115,7 @@
                 </p>
                 <p class="what-is-this">
                     <h2>What is this?</h2>
+                    <p>It's an experiment. Unlike the Five-Minute Geek Show, which is a real grown-up podcast with editing and music, this is just off-the-cuff thoughts and updates.</p>
                     <p>Take a listen to the episode where I describe what the Three-Minute Geek Show is about.<br>(Can't play the episode? Go to the <a href="http://briefs.fm/the-three-minute-geek-show">Briefs.fm page</a> to get old-browser-friendly players)</p>
                     <audio controls preload="metadata" src="https://briefs-production.s3.amazonaws.com/improved_audio/9b322ef3-3742-435d-8acf-ccce5c6a9fe3.mp3">
                         <source src="https://briefs-production.s3.amazonaws.com/improved_audio/9b322ef3-3742-435d-8acf-ccce5c6a9fe3.mp3" type="audio/mpeg">

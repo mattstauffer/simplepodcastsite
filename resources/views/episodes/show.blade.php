2949547425859bfc6f00a92ae06637215111442d
Matt Stauffer <matt@tighten.co>
2016-02-03 14:29:16 -0500

    Extract css

--- resources/views/episodes/show.blade.php
+++ resources/views/episodes/show.blade.php
@@ -1,17 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="font-size: 1.5em;">
-        <div style="text-align: center;"><a href="/">&lt;- All episodes</a></div>
-            
-        <div>
-            <h1>{{ $episode->title }}</h1>
-            <p>{{ $episode->description }}</p>
-            ({{ Carbon\Carbon::parse($episode->pubDate)->format('F j, Y H:i') }})
-            <br><br>
-            <audio controls preload="metadata" src="{{ $episode->guid }}">
-                <source src="{{ $episode->guid }}" type="audio/mpeg">
-            </audio>
-        </div>
+    <div style="text-align: center;"><a href="/">&lt;- All episodes</a></div>
+        
+    <div>
+        <h1>{{ $episode->title }}</h1>
+        <p>{{ $episode->description }}</p>
+        ({{ Carbon\Carbon::parse($episode->pubDate)->format('F j, Y H:i') }})
+        <br><br>
+        <audio controls preload="metadata" src="{{ $episode->guid }}">
+            <source src="{{ $episode->guid }}" type="audio/mpeg">
+        </audio>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $podcast['title'] }}</title>
-        <meta name="description" content="Matt Stauffer, being geeky three minutes at a time.">
+        <meta name="description" content="{{ $podcast['description'] }}">
 
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@3minutegeekshow" />
@@ -22,118 +22,20 @@
         <meta property="og:image:height" content="594" />
         <meta property="og:description" content="Matt Stauffer, being geeky three minutes at a time." />
 
-        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-
-        <style>
-            /* apply a natural box layout model to all elements, but allowing components to change */
-            html {
-                box-sizing: border-box;
-            }
-            *, *:before, *:after {
-                box-sizing: inherit;
-            }
-            html, body {
-                background: #cee7d8;
-                font-family: 'Roboto', sans-serif;
-                height: 100%;
-            }
-
-            img {
-                max-width: 100%;
-            }
-
-            body {
-                display: table;
-                font-weight: 100;
-                margin: 0;
-                padding: 1em;
-                width: 100%;
-            }
-
-            .container {
-                display: table-cell;
-                text-align: center;
-                vertical-align: middle;
-            }
-
-            .content {
-                display: inline-block;
-                text-align: center;
-            }
-
-            .title {
-                display: none;
-            }
-
-            .links {
-                font-size: 1.5em;
-            }
-
-            .links a {
-                color: red;
-            }
-
-            .what-is-this {
-                margin-top: 5em;
-            }
-
-            .footer {
-                margin-top: 4em;
-            }
-
-            @media only screen and (min-width: 500px) {
-                .title {
-                    display: block;
-                    font-size: 6em;
-                    margin-top: 1em;
-                }
-
-                .links {
-                    font-size: 2em;
-                }
-
-                .footer {
-                    margin-top: 6em;
-                }
-            }
-                .episodes-list {
-                    display: block;
-                    margin: 0 auto;
-                    max-width: 30em;
-                    text-align: left;
-                    font-size: 1.25em;
-                }
-                .episodes-title {
-                    font-size: 2em;
-                }
-
-                @media only screen and (min-width: 500px) {
-                    .episodes-list {
-                        font-size: 2em;
-                    }
-                    .episodes-title {
-                        font-size: 4em;
-                        }
-                    .what-is-this {
-                        font-size: 1.5em;
-                        }
-
-                    }
-        </style>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">{{ $podcast['title'] }}</div>
-                <a href="{{ $podcast['link'] }}">
-                    <img src="{{ $podcast['image']->url }}" style="max-width: 50%;">
-                </a>
-                @yield('content')
+        <div class="content">
+            <h1 class="podcast-title">{{ $podcast['title'] }}</h1>
+            <a href="{{ $podcast['link'] }}" class="podcast-logo">
+                <img src="{{ $podcast['image']->url }}">
+            </a>
+            @yield('content')
 
-                <p class="footer">
-                    By <a href="http://twitter.com/stauffermatt">Matt Stauffer</a> | A riff on the <a href="http://fiveminutegeekshow.com/">Five-Minute Geek Show</a> | Powered by <a href="http://briefs.fm/">Briefs.fm</a>
-                </p>
-            </div>
+            <p class="footer">
+                By <a href="http://twitter.com/stauffermatt">Matt Stauffer</a> | A riff on the <a href="http://fiveminutegeekshow.com/">Five-Minute Geek Show</a> | Powered by <a href="http://briefs.fm/">Briefs.fm</a>
+            </p>
         </div>
     </body>
 </html>

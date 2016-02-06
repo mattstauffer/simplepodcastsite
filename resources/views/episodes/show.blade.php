a0f1fb901d45ade59f1faad760bc896b18c8c2b7
Matt Stauffer <matt@tighten.co>
2016-02-06 07:18:51 -0500

    Add Disqus links

--- resources/views/episodes/show.blade.php
+++ resources/views/episodes/show.blade.php
@@ -8,8 +8,29 @@
         <p>{{ $episode->description }}</p>
         ({{ Carbon\Carbon::parse($episode->pubDate)->format('F j, Y H:i') }})
         <br><br>
+
         <audio controls preload="metadata" src="{{ $episode->guid }}">
             <source src="{{ $episode->guid }}" type="audio/mpeg">
         </audio>
+
+        @if (env('DISQUS_KEY'))
+            <h3>Comments</h3>
+            <div id="disqus_thread"></div>
+            <script>
+            var disqus_config = function () {
+                this.page.url = "{{ Request::url() }}";
+                this.page.identifier = "{{ $episode->guid }}";
+            };
+            (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+
+                s.src = '//{{ env('DISQUS_KEY') }}.disqus.com/embed.js';
+
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+        @endif
     </div>
 @endsection

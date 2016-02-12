f0e4ff157fd1291a88b04eb3ca71acb28d82e18a
Matt Stauffer <matt@tighten.co>
2016-02-11 21:25:44 -0500

    add real iTunes link

--- resources/views/customize/links.blade.php
+++ resources/views/customize/links.blade.php
@@ -1,6 +1,6 @@
     <p class="links">
         Subscribe: 
-        <a href="itpc://www.briefs.fm/the-three-minute-geek-show.xml">iTunes</a> |
+        <a href="https://itunes.apple.com/us/podcast/the-three-minute-geek-show/id1083428812">iTunes</a> |
         <a href="overcast://x-callback-url/add?url=https://www.briefs.fm/the-three-minute-geek-show.xml">Overcast</a> |
         <a href="podcast://www.briefs.fm/the-three-minute-geek-show.xml">Podcasts.app</a> | 
         <a href="https://www.briefs.fm/the-three-minute-geek-show.xml">RSS</a> | 

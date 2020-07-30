40f96ad25bc74b2a9de570ed9d216757412bb97b
Matt Stauffer <matt@tighten.co>
2020-07-30 01:16:44 -0400

    Drop briefs reference

--- resources/views/customize/links.blade.php
+++ resources/views/customize/links.blade.php
@@ -1,9 +1,8 @@
     <p class="links">
-        Subscribe: 
+        Subscribe:
         <a href="https://itunes.apple.com/us/podcast/the-three-minute-geek-show/id1083428812">iTunes</a> |
-        <a href="overcast://x-callback-url/add?url=https://www.briefs.fm/the-three-minute-geek-show.xml">Overcast</a> |
-        <a href="podcast://www.briefs.fm/the-three-minute-geek-show.xml">Podcasts.app</a> | 
-        <a href="https://www.briefs.fm/the-three-minute-geek-show.xml">RSS</a> | 
-        <a href="http://briefs.fm/the-three-minute-geek-show">View in Briefs.fm</a>
+        <a href="overcast://x-callback-url/add?url=http://threeminutegeekshow.com/storage/the-three-minute-geek-show.xml">Overcast</a> |
+        <a href="podcast://threeminutegeekshow.com/storage/the-three-minute-geek-show.xml">Podcasts.app</a> |
+        <a href="http://threeminutegeekshow.com/storage/the-three-minute-geek-show.xml">RSS</a>
     </p>
 

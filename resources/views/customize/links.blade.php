2b33f8ce25922f95f27cb12a4f225f38c159eb0d
Matt Stauffer <matt@tighten.co>
2020-08-31 15:40:34 -0400

    Point to .env RSS_URL for link on top of page

--- resources/views/customize/links.blade.php
+++ resources/views/customize/links.blade.php
@@ -3,6 +3,6 @@
         <a href="https://itunes.apple.com/us/podcast/the-three-minute-geek-show/id1083428812">iTunes</a> |
         <a href="overcast://x-callback-url/add?url=http://threeminutegeekshow.com/storage/the-three-minute-geek-show.xml">Overcast</a> |
         <a href="podcast://threeminutegeekshow.com/storage/the-three-minute-geek-show.xml">Podcasts.app</a> |
-        <a href="http://threeminutegeekshow.com/storage/the-three-minute-geek-show.xml">RSS</a>
+        <a href="{{ config('customize.rss_url') }}">RSS</a>
     </p>
 
